3bcbac9ae177947ba80adeea92667a43a589db24
TOM LUCAS <tom.lucas@viacesi.fr>
2021-11-16 09:35:03 +0100

    Ajoutez des fichiers projet.

---
+++ Workshop/Class1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Workshop
+{
+    internal class Modele
+    {
+        protected int[] maj_ASCII = new int[2] { 65, 90 };
+        protected int[] min_ASCII = new int[2] { 97, 122 };
+    }
+
+    internal class View_Modele : Modele
+    {
+    }
+    internal class View
+    {
+    }
+}
