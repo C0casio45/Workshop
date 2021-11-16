9d583d0907a71224398e4ff5099bddb72247e8be
TOM LUCAS <tom.lucas@viacesi.fr>
2021-11-16 10:07:22 +0100

    Instantiation des classes Ecriture code View.execute Ecriture code View_Modele.scaleUP

--- Workshop/Class1.cs
+++ Workshop/Class1.cs
@@ -6,16 +6,41 @@ using System.Threading.Tasks;
 
 namespace Workshop
 {
-    internal class Modele
+    public class Modele
     {
         protected int[] maj_ASCII = new int[2] { 65, 90 };
         protected int[] min_ASCII = new int[2] { 97, 122 };
     }
 
-    internal class View_Modele : Modele
+    public class View_Modele : Modele
     {
+        public string scaleUP(string str)
+        {
+            char[] result = new char[str.Length];
+            int iteration = -1;
+            foreach (char c in str)
+            {
+                int unicode = c;
+                if(unicode >= this.min_ASCII[0] && unicode <= this.min_ASCII[1])
+                {
+                    unicode = (this.min_ASCII[0] - 65) + this.maj_ASCII[0];
+                }
+                result[iteration++] = unicode;  
+            }
+            
+            return str;
+        }
     }
-    internal class View
+    public class View
     {
+        public void execute(string str)
+        {
+            Console.WriteLine("Merci d'entrer une chaine de charactère");
+            Console.ReadLine();
+            var VM = new View_Modele();
+            str = VM.scaleUP(str);
+            Console.WriteLine("Votre nouvelle chaîne de charactère");
+            Console.WriteLine(str);
+        }
     }
 }
